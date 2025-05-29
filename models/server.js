07c330b768f1f2527a1c48eddefb7bca7a30b2a0
wuilmer carbajal <wcarbajal@outlook.com>
2025-05-29 18:54:00 -0500

    2do commit

--- models/server.js
+++ models/server.js
@@ -5,6 +5,7 @@ const http = require( 'http' );
 const socketio = require( 'socket.io' );
 const path = require('path');
 const Sockets = require( './sockets' );
+const cors = require('cors')
 
 
 class Server {
@@ -24,6 +25,9 @@ class Server {
   middlewares() {
     // Desplegar el directorio publico
     this.app.use( express.static( path.resolve( __dirname, '../public') ))
+    
+    // CORS
+    this.app.use( cors() );
   }
 
   configurarSockets() {
